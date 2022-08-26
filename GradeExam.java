0accb66f8e65ecb17d1a93dda0336292ddc470c9
hyesung.park <48369800+hpark7@users.noreply.github.com>
2022-08-25 21:43:57 -0400

    Create GradeExam.java

---
+++ GradeExam.java
@@ -0,0 +1,14 @@
+public class GradeExam {
+    public static void main(String[] args) {
+        char[][] studentAnswers =
+                {{'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                        {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                        {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                        {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                        {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                        {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                        {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                        {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+    }
+}
